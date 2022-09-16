650a11f1190312b34d07259ceead9fe1bf392639
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-09-16 12:11:18 -0300

    final

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 addEventListener('DOMContentLoaded',() => {
-    const btn_menu = document.querySelector('btn_menu')
+    const btn_menu = document.querySelector('.btn_menu')
     if(btn_menu){
         btn_menu.addEventListener('click', () => {
             const menu_items = document.querySelector('.menu_items')
@@ -7,3 +7,9 @@ addEventListener('DOMContentLoaded',() => {
         })
     }
 } )
+
+
+// corregir porque no me haace las media query ( mal puesto el codigo )y porque el menu hambruguesa no hace nada , mal intalado ? cdn ? o mal el link de la class
+
+
+// ACORDARME DE PONER EN EL README FOTO Y VIDEO Y HACERLO CON IMAGEN 
